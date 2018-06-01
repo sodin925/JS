48b24b3c83d96f055ab2477e6926e278f5144ff9
sodin925 <t0975082751@gmail.com>
2018-06-01 13:53:55 +0800

    First

---
+++ script/main.js
@@ -0,0 +1,14 @@
+//button event
+//function Clickcheck(objecj){
+	
+//}
+
+//button
+function button_A(){
+    var a = document.createElement("button");
+    a = document.getElementsByTagName("body");
+    a.innerText = "A";
+    a.onclick = alert("A");
+}
+
+
