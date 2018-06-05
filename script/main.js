4a0495007cf94202451c7e8af1d2ee9b73228ef3
sodin925 <t0975082751@gmail.com>
2018-06-05 23:25:31 +0800

    button

--- script/main.js
+++ script/main.js
@@ -1,14 +1,27 @@
-//button event
-//function Clickcheck(objecj){
-	
-//}
 
-//button
-function button_A(){
-    var a = document.createElement("button");
-    a = document.getElementsByTagName("body");
-    a.innerText = "A";
-    a.onclick = alert("A");
-}
+var buttons = ['A','B','C','D','E'];
+var lastButton = 0;
+
+buttons.forEach(function(button){
+    var newButton = document.createElement("button");
+    newButton.innerText = button;
+    // Click event
+    newButton.onclick = function(event) {
+        lastButton.disabled = false;
+        lastButton = event.target;
+        lastButton.disabled = true;
+        // console.log(lastButton.innerText);
+
+        htmlBody = document.getElementsByTagName("body");
+        text = document.createElement("br")
+        htmlBody[0].append(text);
+        htmlBody[0].append(lastButton.innerText);
+    }
+    // Append it to body
+    htmlBody = document.getElementsByTagName("body");
+    htmlBody[0].append(newButton);
+
+})
+
 
 
